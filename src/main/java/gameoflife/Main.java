2ce5e8c17886e85d88eb3b1b25638175d29d0092
dbusato <davidedbusato@gmail.com>
2021-02-23 18:52:53 +0100

    First impl Main class

--- src/main/java/gameoflife/Main.java
+++ src/main/java/gameoflife/Main.java
@@ -1,7 +1,36 @@
 package gameoflife;
 
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello, Game of Life!");
+import base.graphics.app.GraphAppSoftRendering;
+import base.graphics.app.Settings;
+
+public class Main extends GraphAppSoftRendering {
+	Settings settings;
+
+	public static void main(String[] args) throws Exception {
+		new Main();
+	}
+
+	public Main() throws Exception {
+		settings = new Settings();
+		settings.winWidth = 300;
+		settings.winHeight = 300;
+		settings.title = "Game of Life";
+		settings.showMenu = true;
+		start(settings);
+	}
+
+	@Override
+	public void updateApp(long elapsedTime) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void finishOffApp() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void printFinalStatsApp() {
+		// TODO Auto-generated method stub
 	}
 }
