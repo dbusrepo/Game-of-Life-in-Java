ec48385f5bb96156e20527658790f6e7f2539332
dbusato <davidedbusato@gmail.com>
2021-02-23 19:27:32 +0100

    Change main class name

--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -1,20 +1,19 @@
-package gameoflife;
-
 import base.graphics.app.GraphAppSoftRendering;
 import base.graphics.app.Settings;
 
-public class Main extends GraphAppSoftRendering {
+public class GameOfLife extends GraphAppSoftRendering {
 	Settings settings;
 
 	public static void main(String[] args) throws Exception {
-		new Main();
+		new GameOfLife();
 	}
 
-	public Main() throws Exception {
+	public GameOfLife() throws Exception {
 		settings = new Settings();
 		settings.winWidth = 300;
 		settings.winHeight = 300;
 		settings.title = "Game of Life";
+		settings.showTitleBar = false;
 		settings.showMenu = true;
 		start(settings);
 	}
